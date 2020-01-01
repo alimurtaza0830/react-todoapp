caae83e840adf74ac0090219abb9468117695c59
Ali Murtaza <khattaksa345@gmail.com>
2020-01-01 21:32:50 +0100

    updated the view

--- src/App.js
+++ src/App.js
@@ -69,23 +69,30 @@ class App extends Component {
     })
   }
   render() {
+    const { item, editItem, items } = this.state;
     return (
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-4">
               <h3 className="text-capitalize text-center">
-                React App
+                To Do App
               </h3>
-                <Input 
-                  item={this.state.item} 
-                  handleChange={this.handleChange} 
-                  handleSubmit={this.handleSubmit}
-                  editItem={this.state.editItem} />
-                <TodoList 
-                  items={this.state.items}
-                  clearList={this.clearList}
-                  handleDelete={this.handleDelete}
-                  handleEdit={this.handleEdit} />
+                <div className="col">
+                    <Input 
+                      item={item} 
+                      handleChange={this.handleChange} 
+                      handleSubmit={this.handleSubmit}
+                      editItem={editItem} 
+                    />
+                </div>
+                <div className="col">
+                    <TodoList 
+                      items={items}
+                      clearList={this.clearList}
+                      handleDelete={this.handleDelete}
+                      handleEdit={this.handleEdit} 
+                    />
+                </div>
           </div>
         </div>
       </div>
